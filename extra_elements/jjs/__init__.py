904b8576e2edc1daf41e859902b4f71bc3df7d61
Andrei Kugut <andrey.kugut@gmail.com>
2026-07-15 23:54:55 +0200

    delete second mahattan

--- extra_elements/jjs/__init__.py
+++ extra_elements/jjs/__init__.py
@@ -1,7 +1,6 @@
 from .junction_chain import JunctionChain
 from .junction_manhattan import JunctionManhattan
 from .junction_manhattan_cross import JunctionManhattan_cross
-from .junction_manhattan_for_xmon import JunctionManhattan
 from .junction_topdown import JunctionTopDown
 from .patch import Patch
 from .rectangle_patch import RectanglePatch
